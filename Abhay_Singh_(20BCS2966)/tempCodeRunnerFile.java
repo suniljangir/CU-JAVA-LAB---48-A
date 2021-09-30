db10327949a7cc13d85e636ea46857009e95c476
Abhay Singh <72184269+bhardwaaj@users.noreply.github.com>
2021-09-30 09:58:35 +0530

    Add files via upload

---
+++ Abhay_Singh_(20BCS2966)/tempCodeRunnerFile.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class ClassSet1 {
+	public static int SumOfOddsAndEvens(int n){
+		int n1,n2=0,n3;
+		while(n!=0)
+		{
+			n1=n%10;
+			if((n1%2)!=0)
+				n2+=n1;
+			n/=10;
+		}
+		if(n2%2==0)
+			n3=-1;
+		else
+			n3=1;
+		
+		return n3;	
+	}
+}
+class evenOdd{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+    System.out.print("Drop Your Number :\n");
+    int num = input.nextInt();
+		System.out.println(ClassSet1.SumOfOddsAndEvens(num));
+	}
+}
+
